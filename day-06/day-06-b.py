9db8f62136e415bc1a3450f6b7c08005d1e849bc
Matthew Russell <matt.russell@gearset.com>
2024-12-08 09:37:23 +0000

    Day 6 part 2 - attempt 2 electric boogaloo

--- day-06/day-06-b.py
+++ day-06/day-06-b.py
@@ -6,12 +6,12 @@ class Grid:
     def __init__(self, rows):
 
         # Map from old velocity to new velocity, when the guard turns right
-        self.right_turn_velocity_map = {
-                (-1, 0): ( 0, 1),
-                ( 0, 1): ( 1, 0),
-                ( 1, 0): ( 0,-1),
-                ( 0,-1): (-1, 0),
-            }
+        self.guard_velocities = deque([
+                (-1, 0),
+                ( 0, 1),
+                ( 1, 0),
+                ( 0,-1),
+            ])
 
         self.obstructions = set()
         self.candidates = set()
@@ -36,7 +36,6 @@ class Grid:
                     self.obstructions.add((r, c))
                 elif row[c] == '^':
                     self.guard_position = (r, c)
-                    self.guard_velocity = (-1,0)
                     self.visited_in_direction[0].add(self.guard_position)
             r += 1
         self.height = r
@@ -51,32 +50,80 @@ class Grid:
 
     def walk_guard_forward_until_collision(self):
         while self.guard_is_on_map:
-            #print(f"Guard position: {self.guard_position}; velocity {self.guard_velocity}")
+            print(f"Guard position: {self.guard_position}; velocity {self.guard_velocity}")
             if self.position_in_front_of_guard in self.obstructions:
+                print(f"Found obstruction at {self.position_in_front_of_guard}")
                 return
             self.step_guard_forward()
             self.visited_in_direction[0].add(self.guard_position)
+            print(f"{self.visited_in_direction}")
             if self.guard_position in self.visited_in_direction[1]:
                 print(f"Found candidate: {self.position_in_front_of_guard} while moving {self.guard_velocity}")
                 self.candidates.add(self.position_in_front_of_guard)
 
+    def walk_guard_backward_until_collision(self):
+        while self.guard_is_on_map and self.guard_can_move_back:
+            if self.position_to_left_of_guard in self.obstructions:
+                return
+            self.step_guard_backward()
+            self.visited_in_direction[0].add(self.guard_position)
+
     def step_guard_forward(self):
         self.guard_position = self.position_in_front_of_guard
+        assert self.guard_position not in self.obstructions
+
+    def step_guard_backward(self):
+        self.guard_position = self.position_behind_guard
         assert self.guard_position not in self.obstructions
 
     @property
     def position_in_front_of_guard(self):
         return tuple(c + v for c, v in zip(self.guard_position, self.guard_velocity))
 
+    @property
+    def position_behind_guard(self):
+        return tuple(c + v for c, v in zip(self.guard_position, self.guard_velocities[2]))
+
+    @property
+    def position_to_left_of_guard(self):
+        return tuple(c + v for c, v in zip(self.guard_position, self.guard_velocities[-1]))
+
+    @property
+    def guard_can_move_back(self):
+        return self.position_behind_guard not in self.obstructions
+
+    @property
+    def guard_velocity(self):
+        return self.guard_velocities[0]
+
     def turn_guard_right(self):
-        self.guard_velocity = self.right_turn_velocity_map[self.guard_velocity]
+        self.guard_velocities.rotate(-1)
         self.visited_in_direction.rotate(-1)
+        self.visited_in_direction[0].add(self.guard_position)
+
+    def turn_guard_left(self):
+        self.guard_velocities.rotate(1)
+        self.visited_in_direction.rotate(1)
         self.visited_in_direction[0].add(self.guard_position)
 
 
 if __name__ == '__main__':
     with open('input') as input:
         grid = Grid(l.strip() for l in input.readlines())
+
+    # We need to calculate the path that we could put the guard onto, to get
+    # her back to the start. We're not looking for candidates, here, though
+    initial_position = grid.guard_position
+    while grid.guard_is_on_map and grid.guard_can_move_back:
+        grid.walk_guard_backward_until_collision()
+        grid.turn_guard_left()
+
+    # Reset the guard to the starting position
+    grid.guard_position = initial_position
+    while grid.guard_velocity != (-1, 0):
+        grid.turn_guard_right()
+    # Forget candidates found so far
+    grid.candidates = set()
 
     while grid.guard_is_on_map:
         grid.walk_guard_forward_until_collision()
