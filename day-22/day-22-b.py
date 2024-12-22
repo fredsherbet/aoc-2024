e8d4078a45d532b5e3819af184b5e84fe99b72f1
Matthew Russell <matt.russell@gearset.com>
2024-12-22 20:34:29 +0000

    Fixed day 22 part 2

--- day-22/day-22-b.py
+++ day-22/day-22-b.py
@@ -1,16 +1,17 @@
 from collections import deque
-from itertools import permutations
-import sys
+from itertools import product
 
 class Monkey:
     def __init__(self, initial_secret):
         self.sell_values = calc_sequence(initial_secret)
         self.deltas = [None] + [a-b for a,b in zip(self.sell_values[1:], self.sell_values[:-1])]
+        assert len(self.sell_values) == len(self.deltas)
+        #print(sum(1 for d in self.deltas if d is not None))
         self.sell_map = {}
         signal = deque(maxlen=4)
-        for ix, (d, s) in enumerate(zip(self.deltas, self.sell_values)):
+        for d, s in zip(self.deltas, self.sell_values):
             signal.append(d)
-            if signal[0] != None and tuple(signal) not in self.sell_map:
+            if tuple(signal) not in self.sell_map:
                 self.sell_map[tuple(signal)] = s
 
     def print(self):
@@ -47,16 +48,17 @@ def calc_sequence(initial_secret):
 with open('input') as input:
     monkeys = [Monkey(int(l.strip())) for l in input]
 
-print(sum(m.sell_to((-2,1,-1,3)) for m in monkeys))
-print([m.sell_to((-2,1,-1,3)) for m in monkeys])
+print(f"Loaded {len(monkeys)} monkeys")
+print(sum(m.sell_to((0,-1,1,1)) for m in monkeys))
 
 best = 0
-for signal in permutations(range(-9, 10), 4):
+for signal in product(range(-9, 10), repeat=4):
     sold = sum(m.sell_to(signal) for m in monkeys)
     if sold > best:
         best = sold
         print(f"New best signal {signal}; earns {best}")
-        print(f"{signal} sells {[m.sell_to(signal) for m in monkeys]}")
+        #print(f"{signal} sells {[m.sell_to(signal) for m in monkeys]}")
+
 
 print(best)
 
